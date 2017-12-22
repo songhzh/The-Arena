2702a21359cb1ebd80d6edc5ff11124f3d96423c
rahulminy <rahulminy@live.ca>
2017-12-21 22:23:22 -0700

    New texture for character

--- The Arena/The Arena/Animations.cpp
+++ The Arena/The Arena/Animations.cpp
@@ -4,4 +4,6 @@ void anim::load_vishnu_idle(AnimationContainer & ac)
 {
 	ac.addFrame(vishnu_idle_1, 6);
 	ac.addFrame(vishnu_idle_2, 6);
+	ac.addFrame(vishnu_idle_3, 6);
+	ac.addFrame(vishnu_idle_4, 6);
 }
--- The Arena/The Arena/Animations.h
+++ The Arena/The Arena/Animations.h
@@ -4,8 +4,10 @@
 #include "AnimationContainer.h"
 
 namespace anim {
-	static FrameContainer vishnu_idle_1("Resources/Textures/vish_sheet.png", 2, 1, 0, 0);
-	static FrameContainer vishnu_idle_2("Resources/Textures/vish_sheet.png", 2, 1, 1, 0);
+	static FrameContainer vishnu_idle_1("Resources/Textures/vish_sheet.png", 4, 1, 0, 0);
+	static FrameContainer vishnu_idle_2("Resources/Textures/vish_sheet.png", 4, 1, 1, 0);
+	static FrameContainer vishnu_idle_3("Resources/Textures/vish_sheet.png", 4, 1, 2, 0);
+	static FrameContainer vishnu_idle_4("Resources/Textures/vish_sheet.png", 4, 1, 3, 0);
 
 	void load_vishnu_idle(AnimationContainer& ac);
 }
